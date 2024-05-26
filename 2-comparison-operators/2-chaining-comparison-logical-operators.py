98ba45c1e92fb499351aa3b670dfa6699b6a4d3f
Joel <joel07christ@gmail.com>
2024-05-26 13:09:02 +0530

    feat: comparison and logical operators

---
+++ 2-comparison-operators/2-chaining-comparison-logical-operators.py
@@ -0,0 +1,21 @@
+# Logical Operators
+
+# and
+# or
+# not
+
+print(1 < 2 < 3)
+print(1 < 2 > 3)
+
+# and
+print(1 < 2 and 2 < 3)
+print('h' == 'h' and 2 < 3)
+
+# or
+print(100 == 1 or 2 == 2)
+
+# not
+print(1 == 1)
+print(not 1 == 1)
+print(not(1 == 1))
+print(not(400 > 500))
